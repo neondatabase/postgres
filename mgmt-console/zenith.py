e5447e2cddfa7dfe6557b2cfcf6520beaccc2048
anastasia <a.lubennikova@postgrespro.ru>
2021-03-10 16:57:01 +0300

    [issue #7] CLI. Implement basic pg operations using helpers from testgres

--- mgmt-console/zenith.py
+++ mgmt-console/zenith.py
@@ -1,6 +1,12 @@
 #zenith.py
 import click
+import testgres
+import os
 
+from testgres import PostgresNode
+from tabulate import tabulate
+
+zenith_base_dir = '/home/anastasia/zenith/basedir'
 
 @click.group()
 def main():
@@ -8,40 +14,85 @@ def main():
 
 @click.group()
 def pg():
-    """Db operations"""
+    """Db operations
+
+        NOTE: 'database' here means one postgresql node
+    """
 
 @click.command(name='create')
-@click.argument('name')
+@click.option('--name', required=True)
 @click.option('-s', '--storage-name', help='Name of the storage',
                                  default='zenith-local',
                                  show_default=True)
 @click.option('--snapshot', help='init from the snapshot. Snap is a name or URL')
-def pg_create(name, storage_name, snapshot):
+@click.option('--no-start', is_flag=True, help='Do not start created node',
+                            default=False, show_default=True)
+def pg_create(name, storage_name, snapshot, no_start):
     """Initialize the database"""
-
-@click.command(name='destroy')
-def pg_destroy():
-    """Drop the database"""
+    node = PostgresNode()
+    base_dir = os.path.join(zenith_base_dir, 'pg', name)
+    node = testgres.get_new_node(name, base_dir=base_dir)
+    # TODO skip init, instead of that link node with storage or upload it from snapshot
+    node.init()
+    if(no_start==False):
+        node.start()
 
 @click.command(name='start')
+@click.option('--name', required=True)
+@click.option('--snapshot')
 @click.option('--read-only', is_flag=True, help='Start read-only node', show_default=True)
-def pg_start(snapshot):
+def pg_start(name, snapshot, read_only):
     """Start the database"""
-    click.echo('snapshot %s' % snapshot)
+    node = PostgresNode()
+    base_dir = os.path.join(zenith_base_dir, 'pg', name)
+    node = testgres.get_new_node(name, base_dir=base_dir)
+    # TODO pass snapshot as a parameter
+    node.start()
 
 @click.command(name='stop')
-def pg_stop():
+@click.option('--name', required=True)
+def pg_stop(name):
     """Stop the database"""
+    node = PostgresNode()
+    base_dir = os.path.join(zenith_base_dir, 'pg', name)
+    node = testgres.get_new_node(name, base_dir=base_dir)
+    node.stop()
+
+@click.command(name='destroy')
+@click.option('--name', required=True)
+def pg_destroy(name):
+    """Drop the database"""
+    node = PostgresNode()
+    base_dir = os.path.join(zenith_base_dir, 'pg', name)
+    node = testgres.get_new_node(name, base_dir=base_dir)
+    node.cleanup()
 
 @click.command(name='list')
 def pg_list():
     """List existing databases"""
+    dirs = os.listdir(os.path.join(zenith_base_dir, 'pg'))
+    path={}
+    status={}
+    data=[]
+
+    for dirname in dirs:
+        path[dirname] = os.path.join(zenith_base_dir, 'pg', dirname)
+        fname = os.path.join( path[dirname], 'data/postmaster.pid')
+        try:
+            f = open(fname,'r')
+            status[dirname] = f.readlines()[-1]
+        except OSError as err:
+            status[dirname]='inactive'
+        data.append([dirname , status[dirname], path[dirname]])
+
+    print(tabulate(data, headers=['Name', 'Status', 'Path']))
 
 pg.add_command(pg_create)
 pg.add_command(pg_destroy)
 pg.add_command(pg_start)   
 pg.add_command(pg_stop)   
-pg.add_command(pg_list)   
+pg.add_command(pg_list)
+
 
 
 @click.group()
@@ -49,14 +100,14 @@ def storage():
     """Storage operations"""
 
 @click.command(name='attach')
-@click.argument('name')
-def storage_attach():
+@click.option('--name')
+def storage_attach(name):
     """Attach the storage"""
 
 @click.command(name='detach')
-@click.argument('name')
+@click.option('--name')
 @click.option('--force', is_flag=True, show_default=True)
-def storage_detach():
+def storage_detach(name):
     """Detach the storage"""
 
 @click.command(name='list')
@@ -70,7 +121,6 @@ storage.add_command(storage_list)
 @click.group()
 def snapshot():
     """Snapshot operations"""
-
 
 @click.command(name='create')
 def snapshot_create():
