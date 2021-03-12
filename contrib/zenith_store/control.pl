b35413ad40c0bf77d4f5b15a5c326182ddc67bc0
Stas Kelvich <stanconn@gmail.com>
2021-03-12 11:59:55 +0300

    Makefile support for running pg_regress over compute+pageserver

--- contrib/zenith_store/control.pl
+++ contrib/zenith_store/control.pl
@@ -15,23 +15,38 @@ if ($action eq "start")
 	$PostgresNode::last_port_assigned = 65431;
 
 	my $node_primary = get_new_node('primary');
-	$node_primary->init(allows_streaming => 1);
+	my $primary_connstr = $node_primary->connstr('postgres');
+	$primary_connstr =~ s/\'//g;
+
+	my $page_server = get_new_node('pageserver');
+	my $pager_connstr = $page_server->connstr('postgres');
+	$pager_connstr =~ s/\'//g;
+
+	#
+	# Initialize primary node
+	#
+	$node_primary->init(
+		allows_streaming => 1
+	);
 	$node_primary->append_conf('postgresql.conf', qq{
-		max_connections = 50
+		log_line_prefix = '%m [%p] [xid%x] %i '
+		log_statement = all
+		page_server_connstring = '$pager_connstr'
+		shared_buffers = 1MB
 	});
 	$node_primary->start;
 	$node_primary->safe_psql("postgres", "SELECT pg_create_physical_replication_slot('zenith_store', true)");
 	$node_primary->safe_psql('postgres', "CREATE DATABASE regression");
 
-	my $connstr = ($node_primary->connstr('postgres'));
-	$connstr =~ s/\'//g;
-	my $page_server = get_new_node('pageserver');
+	#
+	# Initialize page store
+	#
 	$page_server->init;
 	$page_server->append_conf('postgresql.conf', qq{
 		log_line_prefix = '%m [%p] [xid%x] %i '
 		log_statement = all
 		shared_preload_libraries = 'zenith_store'
-		zenith_store.connstr = '$connstr'
+		zenith_store.connstr = '$primary_connstr'
 	});
 	$page_server->start;
 	$page_server->safe_psql("postgres", "CREATE EXTENSION zenith_store");
@@ -40,7 +55,10 @@ if ($action eq "start")
 }
 elsif ($action eq "stop")
 {
-	# TODO
+	foreach my $pg (<$TestLib::tmp_check/*data>) {
+		TestLib::system_log('pg_ctl', '-D', "$pg/pgdata",
+							'-m', 'fast', 'stop');
+	}
 }
 else
 {
