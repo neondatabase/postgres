3eb56d17b6a61ac49c118351465f2eb8f6dc75b4
Konstantin Knizhnik <knizhnik@garret.ru>
2021-03-20 13:00:01 +0300

    Fix bug in LSN comparison function

--- src/bin/safekeeper/utils.c
+++ src/bin/safekeeper/utils.c
@@ -312,7 +312,7 @@ CompareLsn(const void *a, const void *b)
 	XLogRecPtr	lsn1 = *((const XLogRecPtr *) a);
 	XLogRecPtr	lsn2 = *((const XLogRecPtr *) b);
 
-	if (lsn1 > lsn2)
+	if (lsn1 < lsn2)
 		return -1;
 	else if (lsn1 == lsn2)
 		return 0;
