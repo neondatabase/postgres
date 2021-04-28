0a9dfa3bbbfcf1a2b394bd6349d221c2508f8b59
Konstantin Knizhnik <knizhnik@garret.ru>
2021-04-28 20:01:08 +0300

    Fix zenith-clog test

--- src/test/regress/sql/zenith-clog.sql
+++ src/test/regress/sql/zenith-clog.sql
@@ -3,7 +3,7 @@ declare
     xid xid8;
 	i integer;
 begin
-    for i in 1..1000000 loop
+    for i in 1..10000 loop
 	    xid = txid_current();
 		commit;
 		if (pg_xact_status(xid) <> 'committed') then
@@ -13,4 +13,4 @@ begin
 end;
 $$ language plpgsql;
 
-select do_commits();
+call do_commits();
