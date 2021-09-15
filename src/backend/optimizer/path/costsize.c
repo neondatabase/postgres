82d4f3981260f8350b92a12698d0bfbe36ec21b3
Konstantin Knizhnik <knizhnik@garret.ru>
2021-09-15 11:46:56 +0300

    Add seqscan_prefetch_buffers GUC to specify number of prefetched pages for seqscan

--- src/backend/optimizer/path/costsize.c
+++ src/backend/optimizer/path/costsize.c
@@ -152,6 +152,7 @@ bool		enable_partition_pruning = true;
 bool		enable_async_append = true;
 bool        enable_seqscan_prefetch = true;
 bool        enable_indexscan_prefetch = true;
+int         seqscan_prefetch_buffers = 0;
 
 typedef struct
 {
