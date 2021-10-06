58aa3d04639bd1bad8e08502d209b11f922f1c47
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2021-10-06 10:57:00 +0300

    Store unlogged tables locally, and replace PD_WAL_LOGGED.
    The smgr implementation needs to distinguish between unlogged/temp and
    regular 'permanent' relations, but the smgr API doesn't currently include
    that information. Add a 'relpersistence' field to SmgrRelationData, and
    as an argument to smgropen(). However, not all callers of smgropen()
    have a relcache entry at hand, so we allow some operations to pass 0,
    meaning 'unknown'.
    
    Now that we can store unlogged tables locally, use the same machinery
    to handle the buffered GiST and SP-GiST index builds. They populate the
    index by inserting all the tuples, and use the shared buffer cache while
    they do that. They don't WAL-log the pages while they do that, they log
    the whole relation as a separate bulk operation after the build has
    finished. That poses a problem for Zenith, where smgrwrite() is a no-op
    and we rely on WAL-logging to reconstruct the pages. Solve that problem by
    storing the pages locally in the compute node, like an unlogged relation,
    until the index build finishes and all the pages have been WAL-logged.
    To do that, the smgr needs to know when the caller is an unlogged build
    operation like that, so add functions to the Smgr API for that.
    
    With this commit, we no longer generate an FPI record whenever a rel is
    extended with an all-zeros page. See github issue #482. That greatly
    reduces the amount of WAL generated during bulk loading.

--- src/include/storage/bufmgr.h
+++ src/include/storage/bufmgr.h
@@ -227,6 +227,8 @@ extern void BufferGetTag(Buffer buffer, RelFileNode *rnode,
 
 extern void MarkBufferDirtyHint(Buffer buffer, bool buffer_std);
 
+extern void MarkBufferPermanent(Buffer buffer);
+
 extern void UnlockBuffers(void);
 extern void LockBuffer(Buffer buffer, int mode);
 extern bool ConditionalLockBuffer(Buffer buffer);
