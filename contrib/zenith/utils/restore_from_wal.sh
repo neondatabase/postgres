12f4e4e9e689b2cfcaed7c6cf2fb06cfe7a2a08f
Konstantin Knizhnik <knizhnik@garret.ru>
2022-01-28 10:30:17 +0300

    Calculate wal position in restore_from_wal scripts
    refer #1169

--- contrib/zenith/utils/restore_from_wal.sh
+++ contrib/zenith/utils/restore_from_wal.sh
@@ -2,12 +2,13 @@ WAL_PATH=$1
 SYSID=`od -A n -j 24 -N 8 -t d8 $WAL_PATH/000000010000000000000002* | cut -c 3-`
 rm -fr pgsql.0
 env -i /home/knizhnik/zenith.main/tmp_install/bin/initdb -E utf8 -U zenith_admin -D pgsql.0 --sysid=$SYSID
+REDO_POS=0x`pg_controldata -D pgsql.0 | fgrep "REDO location"| cut -c 42-`
+declare -i WAL_SIZE=$REDO_POS+114
 pg_ctl -D pgsql.0 -l logfile start
 pg_ctl -D pgsql.0 -l logfile stop -m immediate
 cp pgsql.0/pg_wal/000000010000000000000001 .
 cp $WAL_PATH/* pgsql.0/pg_wal/
 (cd pgsql.0/pg_wal ; for partial in *.partial ; do mv $partial `basename $partial .partial`; done)
-dd if=000000010000000000000001 of=pgsql.0/pg_wal/000000010000000000000001 bs=6924704 count=1 conv=notrunc
-rm 000000010000000000000001
+dd if=000000010000000000000001 of=pgsql.0/pg_wal/000000010000000000000001 bs=$WAL_SIZE count=1 conv=notrunc
 rm -f logfile 000000010000000000000001
 pg_ctl -D pgsql.0 -l logfile start
--- contrib/zenith/utils/restore_from_wal_archive.sh
+++ contrib/zenith/utils/restore_from_wal_archive.sh
@@ -3,9 +3,11 @@ SYSID=`od -A n -j 24 -N 8 -t d8 $WAL_PATH/000000010000000000000002* | cut -c 3-`
 rm -fr pgsql.0 /tmp/pg_wals
 mkdir /tmp/pg_wals
 env -i /home/knizhnik/zenith.main/tmp_install/bin/initdb -E utf8 -U zenith_admin -D pgsql.0 --sysid=$SYSID
+REDO_POS=0x`pg_controldata -D pgsql.0 | fgrep "REDO location"| cut -c 42-`
+declare -i WAL_SIZE=$REDO_POS+114
 cp $WAL_PATH/* /tmp/pg_wals
 (cd /tmp/pg_wals ; for partial in *.partial ; do mv $partial `basename $partial .partial`; done)
-dd if=pgsql.0/pg_wal/000000010000000000000001 of=/tmp/pg_wals/000000010000000000000001 bs=6924704 count=1 conv=notrunc
+dd if=pgsql.0/pg_wal/000000010000000000000001 of=/tmp/pg_wals/000000010000000000000001 bs=$WAL_SIZE count=1 conv=notrunc
 echo > pgsql.0/recovery.signal
 rm -f pgsql.0/pg_wal/*
 echo "restore_command = 'cp /tmp/pg_wals/%f %p'" >> pgsql.0/postgresql.conf
