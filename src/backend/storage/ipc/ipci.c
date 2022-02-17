0fae0673cd1b06ad7bac5eb62d9b2cd18463bcee
anastasia <lubennikovaav@gmail.com>
2022-02-17 19:16:49 +0300

    Add backpressure_lsns() function. Fix zenith feedback processing

--- src/backend/storage/ipc/ipci.c
+++ src/backend/storage/ipc/ipci.c
@@ -34,6 +34,7 @@
 #include "replication/slot.h"
 #include "replication/walreceiver.h"
 #include "replication/walsender.h"
+#include "replication/walproposer.h"
 #include "storage/bufmgr.h"
 #include "storage/dsm.h"
 #include "storage/ipc.h"
@@ -150,6 +151,9 @@ CreateSharedMemoryAndSemaphores(void)
 		size = add_size(size, BTreeShmemSize());
 		size = add_size(size, SyncScanShmemSize());
 		size = add_size(size, AsyncShmemSize());
+
+		size = add_size(size, ZenithFeedbackShmemSize());
+
 #ifdef EXEC_BACKEND
 		size = add_size(size, ShmemBackendArraySize());
 #endif
@@ -269,6 +273,8 @@ CreateSharedMemoryAndSemaphores(void)
 	BTreeShmemInit();
 	SyncScanShmemInit();
 	AsyncShmemInit();
+
+	ZenithFeedbackShmemInit();
 
 #ifdef EXEC_BACKEND
 
