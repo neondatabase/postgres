9d07fe9f58032f96ecbb57d10151043f53846b1b
Anton Shyrabokau <97127717+antons-antons@users.noreply.github.com>
2022-02-18 08:52:01 -0800

    Expose reading a relation page at a specific LSN (#131)
    * Expose reading a relation page at a specific LSN
    
    * Addressing comments

--- contrib/zenith_test_utils/zenith_test_utils--1.0.sql
+++ contrib/zenith_test_utils/zenith_test_utils--1.0.sql
@@ -12,3 +12,13 @@ RETURNS VOID
 AS 'MODULE_PATHNAME', 'clear_buffer_cache'
 LANGUAGE C STRICT
 PARALLEL UNSAFE;
+
+CREATE FUNCTION get_raw_page_at_lsn(relname text, forkname text, blocknum int8, lsn pg_lsn)
+RETURNS bytea
+AS 'MODULE_PATHNAME', 'get_raw_page_at_lsn'
+LANGUAGE C PARALLEL UNSAFE;
+
+CREATE FUNCTION get_raw_page_at_lsn(tbspc oid, db oid, relfilenode oid, forknum int8, blocknum int8, lsn pg_lsn)
+RETURNS bytea
+AS 'MODULE_PATHNAME', 'get_raw_page_at_lsn_ex'
+LANGUAGE C PARALLEL UNSAFE;
