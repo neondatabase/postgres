4ffb3bd85e1c7fc381c01af9c2e1749a477fcfce
Konstantin Knizhnik <knizhnik@garret.ru>
2022-03-15 14:11:00 +0300

    Update scripts for recovery from WAL

--- contrib/zenith/utils/restore_from_wal_archive.sh
+++ contrib/zenith/utils/restore_from_wal_archive.sh
@@ -1,16 +1,20 @@
-WAL_PATH=$1
+PG_BIN=$1
+WAL_PATH=$2
+OUTPUT_DIR=$3
+DATA_DIR=$OUTPUT_DIR/pgdata-vanilla
+echo WAL_PATh=$WAL_PATH OUTPUT_DIR=$OUTPUT_DIR DATA_DIR=$DATA_DIR
 SYSID=`od -A n -j 24 -N 8 -t d8 $WAL_PATH/000000010000000000000002* | cut -c 3-`
-rm -fr pgsql.0 /tmp/pg_wals
+rm -fr $DATA_DIR /tmp/pg_wals
 mkdir /tmp/pg_wals
-INITDB=`type -p initdb`
-env -i $INITDB -E utf8 -U zenith_admin -D pgsql.0 --sysid=$SYSID
-REDO_POS=0x`pg_controldata -D pgsql.0 | fgrep "REDO location"| cut -c 42-`
+env -i $PG_BIN/initdb -E utf8 -U zenith_admin -D $DATA_DIR --sysid=$SYSID
+REDO_POS=0x`$PG_BIN/pg_controldata -D $DATA_DIR | fgrep "REDO location"| cut -c 42-`
 declare -i WAL_SIZE=$REDO_POS+114
 cp $WAL_PATH/* /tmp/pg_wals
-(cd /tmp/pg_wals ; for partial in *.partial ; do mv $partial `basename $partial .partial`; done)
-dd if=pgsql.0/pg_wal/000000010000000000000001 of=/tmp/pg_wals/000000010000000000000001 bs=$WAL_SIZE count=1 conv=notrunc
-echo > pgsql.0/recovery.signal
-rm -f pgsql.0/pg_wal/*
-echo "restore_command = 'cp /tmp/pg_wals/%f %p'" >> pgsql.0/postgresql.conf
-rm -f logfile
-pg_ctl -D pgsql.0 -l logfile start
+if [ -f $DATA_DIR/pg_wal/*.partial ]
+then
+	(cd /tmp/pg_wals ; for partial in \*.partial ; do  mv $partial `basename $partial .partial` ; done)
+fi
+dd if=$DATA_DIR/pg_wal/000000010000000000000001 of=/tmp/pg_wals/000000010000000000000001 bs=$WAL_SIZE count=1 conv=notrunc
+echo > $DATA_DIR/recovery.signal
+rm -f $DATA_DIR/pg_wal/*
+echo "restore_command = 'cp /tmp/pg_wals/%f %p'" >> $DATA_DIR/postgresql.conf
