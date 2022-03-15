cc12d19a79e7814be3128b2e321fa98eb113e73a
Konstantin Knizhnik <knizhnik@garret.ru>
2022-03-15 18:05:54 +0300

    Make it possible to specify port and datadir for restore_from_wal

--- contrib/zenith/utils/restore_from_wal.sh
+++ contrib/zenith/utils/restore_from_wal.sh
@@ -1,10 +1,11 @@
 PG_BIN=$1
 WAL_PATH=$2
-OUTPUT_DIR=$3
-DATA_DIR=$OUTPUT_DIR/pgdata-vanilla
+DATA_DIR=$3
+PORT=$4
 SYSID=`od -A n -j 24 -N 8 -t d8 $WAL_PATH/000000010000000000000002* | cut -c 3-`
 rm -fr $DATA_DIR
 env -i $PG_BIN/initdb -E utf8 -D $DATA_DIR --sysid=$SYSID
+echo port=$PORT >> $DATA_DIR/postgresql.conf
 REDO_POS=0x`$PG_BIN/pg_controldata -D $DATA_DIR | fgrep "REDO location"| cut -c 42-`
 declare -i WAL_SIZE=$REDO_POS+114
 $PG_BIN/pg_ctl -D $DATA_DIR -l logfile start
