4765c494740d842ce4c762bc6cf43945cc91df1b
Cuong Nguyen <ctring23@gmail.com>
2022-03-14 20:23:13 -0400

    Tell page server to connect to other regions

--- contrib/neon/multiregion.c
+++ contrib/neon/multiregion.c
@@ -36,10 +36,7 @@
 /* GUCs */
 int zenith_current_region;
 static char *zenith_region_timelines;
-static char *zenith_region_connstrings;
-
-static List *region_timelines = NULL;
-static List *region_connstrings = NULL;
+static char *zenith_region_safekeeper_addrs;
 
 static bool
 check_zenith_region_timelines(char **newval, void **extra, GucSource source)
@@ -82,98 +79,50 @@ check_zenith_region_timelines(char **newval, void **extra, GucSource source)
 	return true;
 }
 
-static void
-assign_zenith_region_timelines(const char *newval, void *extra)
-{
-	char *rawstring;
-	List *timelines;
-
-	/* Need a modifiable copy of string */
-	rawstring = pstrdup(newval);
-
-	/* Parse string into list of timelines */
-	if (!SplitIdentifierString(rawstring, ',', &timelines))
-	{
-		pfree(rawstring);
-		list_free(timelines);
-		return;
-	}
-
-	/* 
-	 * The check_zenith_region_timelines function already did all
-	 * the checks so assign immediately 
-	 */
-	region_timelines = timelines;
-
-	pfree(rawstring);
-}
-
 static bool
-check_zenith_region_connstrings(char **newval, void **extra, GucSource source)
+check_zenith_region_safekeeper_addrs(char **newval, void **extra, GucSource source)
 {
 	char *rawstring;
-	List *connstrings;
+	List *safekeeper_addrs;
 
 	/* Need a modifiable copy of string */
 	rawstring = pstrdup(*newval);
 
-	/* Parse string into list of connstrings */
-	if (!SplitIdentifierString(rawstring, ',', &connstrings))
+	/* Parse string into list of safekeeper_addrs */
+	if (!SplitIdentifierString(rawstring, ',', &safekeeper_addrs))
 	{
 		/* syntax error in list */
 		GUC_check_errdetail("List syntax is invalid.");
 		pfree(rawstring);
-		list_free(connstrings);
+		list_free(safekeeper_addrs);
 		return false;
 	}
 
 	pfree(rawstring);
-	list_free(connstrings);
+	list_free(safekeeper_addrs);
 
 	return true;
-}
-
-static void
-assign_zenith_region_connstrings(const char *newval, void *extra)
-{
-	char *rawstring;
-	List *connstrings;
-
-	/* Need a modifiable copy of string */
-	rawstring = pstrdup(newval);
-
-	/* Parse string into list of connstrings */
-	if (!SplitIdentifierString(rawstring, ',', &connstrings))
-	{
-		pfree(rawstring);
-		list_free(connstrings);
-		return;
-	}
-
-	region_connstrings = connstrings;
-
-	pfree(rawstring);
 }
 
 void DefineMultiRegionCustomVariables(void)
 {
 	DefineCustomStringVariable("zenith.region_timelines",
-								"List of timelineids corresponding to the partitions",
+								"List of timelineids corresponding to the partitions. The first timeline is always for the global partition.",
 								NULL,
 								&zenith_region_timelines,
 								"",
 								PGC_POSTMASTER,
 								0, /* no flags required */
-								check_zenith_region_timelines, assign_zenith_region_timelines, NULL);
+								check_zenith_region_timelines, NULL, NULL);
 
-	DefineCustomStringVariable("zenith.region_connstrings",
-								"List of connstrings corresponding to the partitions",
+	DefineCustomStringVariable("zenith.region_safekeeper_addrs",
+								"List of addresses to the safekeepers in every regions. The first address is always for the global partition",
 								NULL,
-								&zenith_region_connstrings,
+								&zenith_region_safekeeper_addrs,
 								"",
 								PGC_POSTMASTER,
 								0, /* no flags required */
-								check_zenith_region_connstrings, assign_zenith_region_connstrings, NULL);
+								check_zenith_region_safekeeper_addrs, NULL, NULL);
 
 	DefineCustomIntVariable("zenith.current_region",
 							"ID of the current region",
@@ -189,7 +138,33 @@ void DefineMultiRegionCustomVariables(void)
 bool zenith_multiregion_enabled(void)
 {
 	return zenith_region_timelines && zenith_region_timelines[0] &&
-		zenith_region_connstrings && zenith_region_connstrings[0];
+		zenith_region_safekeeper_addrs && zenith_region_safekeeper_addrs[0];
+}
+
+static bool
+split_into_host_and_port(const char *addr, char** host, char** port)
+{
+	int 		hostlen;
+	int 		portlen;
+	const char 	*colon = strchr(addr, ':');
+
+	if (colon == NULL)
+		return false;
+
+	hostlen = colon - addr;
+	portlen = strlen(addr) - hostlen - 1;
+
+	*host = (char *)palloc(hostlen + 1);
+	*port = (char *)palloc(portlen + 1);
+	if (*host == NULL || *port == NULL)
+		return false;
+
+	strncpy(*host, addr, hostlen);
+	(*host)[hostlen] = '\0';
+	strncpy(*port, colon + 1, portlen);
+	(*port)[portlen] = '\0';
+
+	return true;
 }
 
 /*
@@ -198,12 +173,14 @@ bool zenith_multiregion_enabled(void)
  */
 void zenith_multiregion_connect(PGconn **pageserver_conn, bool *connected)
 {
+	List *timelines;
+	ListCell *lc_timeline;
+	List *safekeeper_addrs;
+	ListCell *lc_addr;
 	char *query;
 	int ret;
 
 	Assert(!*connected);
-	Assert(region_timelines);
-	Assert(region_connstrings);
 
 	*pageserver_conn = PQconnectdb(page_server_connstring);
 
@@ -219,29 +196,58 @@ void zenith_multiregion_connect(PGconn **pageserver_conn, bool *connected)
 				errdetail_internal("%s", msg)));
 	}
 
-	/* 
-	 * Ask the Page Server to connect to us, and stream WAL from us. 
-	 * TODO: Connect to the safekeeper in other regions
-	 */
-	// if (callmemaybe_connstring && callmemaybe_connstring[0] && zenith_tenant && zenith_timeline)
-	// {
-	// 	PGresult *res;
+	/* These strings are already syntax-checked by GUC hooks */
+	Assert(SplitIdentifierString(pstrdup(zenith_region_timelines), ',', &timelines));
+	Assert(SplitIdentifierString(pstrdup(zenith_region_safekeeper_addrs), ',', &safekeeper_addrs));
 
-	// 	query = psprintf("callmemaybe %s %s %s", zenith_tenant, zenith_timeline, callmemaybe_connstring);
-	// 	res = PQexec(*pageserver_conn, query);
-	// 	if (PQresultStatus(res) != PGRES_COMMAND_OK)
-	// 	{
-	// 		PQfinish(*pageserver_conn);
-	// 		*pageserver_conn = NULL;
-	// 		zenith_log(ERROR,
-	// 				   "[ZENITH_SMGR] callmemaybe command failed");
-	// 	}
-	// 	PQclear(res);
-	// }
+	/* Make sure that they are the same length */
+	Assert(list_length(timelines) == list_length(safekeeper_addrs));
+
+	/* 
+	 * Ask the Page Server to connect to other regions
+	 */
+	// FIXME(ctring): neon no longer uses callmemaybe
+	forboth (lc_timeline, timelines, lc_addr, safekeeper_addrs)
+	{
+		PGresult *res;
+		int i = foreach_current_index(lc_timeline);
+		char *timeline = (char *)lfirst(lc_timeline);
+		char *addr = (char *)lfirst(lc_addr);
+		char *host, *port;
+
+		/* Skip the global partition and current partition */
+		if (i == 0 || i == zenith_current_region)
+			continue;
+		
+		if (!split_into_host_and_port(addr, &host, &port))
+		{
+			PQfinish(*pageserver_conn);
+			*pageserver_conn = NULL;
+			zenith_log(ERROR, "[ZENITH_SMGR] invalid safekeeper address \"%s\"", addr);
+		}
+
+		/* Connection string format taken from callmemaybe.rs in walkeeper */
+		query = psprintf(
+			"callmemaybe %s %s host=%s port=%s options='-c ztenantid=%s ztimelineid=%s pageserver_connstr=%s'",
+			zenith_tenant, timeline, host, port, zenith_tenant, timeline, page_server_connstring);
+
+		pfree(host);
+		pfree(port);
+
+		res = PQexec(*pageserver_conn, query);
+		if (PQresultStatus(res) != PGRES_COMMAND_OK)
+		{
+			PQfinish(*pageserver_conn);
+			*pageserver_conn = NULL;
+			zenith_log(ERROR, "[ZENITH_SMGR] callmemaybe command failed for timeline %s", timeline);
+		}
+		PQclear(res);
+	}
+
+	list_free(timelines);
+	list_free(safekeeper_addrs);
 
 	query = psprintf("multipagestream %s %s", zenith_tenant, zenith_region_timelines);
-	zenith_log(LOG, "timelines: %s", zenith_region_timelines);
-
 	ret = PQsendQuery(*pageserver_conn, query);
 	if (ret != 1)
 	{
