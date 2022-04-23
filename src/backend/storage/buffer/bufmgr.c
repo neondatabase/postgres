14a5bc8c6e2278da8b387e3be3ec79feb02696eb
Konstantin Knizhnik <knizhnik@garret.ru>
2022-04-23 08:52:15 +0300

    Avoid redundand memory allocation and sycnhronization in walredo (#144)
    * Avoid redundand memory allocation and sycnhronization in walredo
    
    * Address review comments
    
    * Reduce number of temp buffers and size of inmem file storage for wal redo postgres
    
    * Misc cleanup
    
    Add comments on 'inmem_smgr.c', remove superfluous copy-pasted comments,
    pgindent.
    
    Co-authored-by: Heikki Linnakangas <heikki.linnakangas@iki.fi>

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -810,7 +810,6 @@ ReadBufferWithoutRelcache(RelFileNode rnode, ForkNumber forkNum,
 							 mode, strategy, &hit);
 }
 
-
 /*
  * ReadBuffer_common -- common logic for all ReadBuffer variants
  *
@@ -825,7 +824,11 @@ ReadBuffer_common(SMgrRelation smgr, char relpersistence, ForkNumber forkNum,
 	Block		bufBlock;
 	bool		found;
 	bool		isExtend;
-	bool		isLocalBuf = SmgrIsTemp(smgr);
+	/*
+	 * wal_redo postgres is working in single user mode, we do not need to synchronize access to shared buffer, 
+	 * so let's use local buffers instead
+	 */
+	bool		isLocalBuf = SmgrIsTemp(smgr) || am_wal_redo_postgres;
 
 	*hit = false;
 
