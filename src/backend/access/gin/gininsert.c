68dbff97c5c260b9bd7089a9bf8fa0eff0581bdf
Konstantin Knizhnik <knizhnik@garret.ru>
2022-07-09 16:30:41 +0300

    Large last written lsn cache (#177)
    Maintain cache of last written LSN for each relation segment (8 Mb).

--- src/backend/access/gin/gininsert.c
+++ src/backend/access/gin/gininsert.c
@@ -421,8 +421,8 @@ ginbuild(Relation heap, Relation index, IndexInfo *indexInfo)
 		log_newpage_range(index, MAIN_FORKNUM,
 						  0, RelationGetNumberOfBlocks(index),
 						  true);
+		SetLastWrittenLSN(XactLastRecEnd, index->rd_smgr->smgr_rnode.node.relNode, 0, RelationGetNumberOfBlocks(index));
 	}
-	SetLastWrittenPageLSN(XactLastRecEnd);
 
 	smgr_end_unlogged_build(index->rd_smgr);
 
