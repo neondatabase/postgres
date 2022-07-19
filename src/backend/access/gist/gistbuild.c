a96326cef8e9c1c629c97f40f724c13f864e3b18
Konstantin Knizhnik <knizhnik@garret.ru>
2022-07-19 07:50:00 +0300

    Update last written LSN for gin/gist index metadata (#182)
    * Update last written LSN for gin/gist index metadata
    
    * Replace SetLastWrittenLSN with family of SetLastWrittenLSNFFor* functions

--- src/backend/access/gist/gistbuild.c
+++ src/backend/access/gist/gistbuild.c
@@ -335,9 +335,10 @@ gistbuild(Relation heap, Relation index, IndexInfo *indexInfo)
 			log_newpage_range(index, MAIN_FORKNUM,
 							  0, RelationGetNumberOfBlocks(index),
 							  true);
-			SetLastWrittenLSN(XactLastRecEnd,
+			SetLastWrittenLSNForBlockRange(XactLastRecEnd,
 							  index->rd_smgr->smgr_rnode.node.relNode,
 							  0, RelationGetNumberOfBlocks(index));
+			SetLastWrittenLSNForRelation(XactLastRecEnd, index->rd_smgr->smgr_rnode.node.relNode);
 		}
 		smgr_end_unlogged_build(index->rd_smgr);
 	}
@@ -468,8 +469,9 @@ gist_indexsortbuild(GISTBuildState *state)
 
 		lsn = log_newpage(&state->indexrel->rd_node, MAIN_FORKNUM, GIST_ROOT_BLKNO,
 					pagestate->page, true);
-		SetLastWrittenLSN(lsn, state->indexrel->rd_smgr->smgr_rnode.node.relNode,
-							  GIST_ROOT_BLKNO, GIST_ROOT_BLKNO);
+		SetLastWrittenLSNForBlock(lsn, state->indexrel->rd_smgr->smgr_rnode.node.relNode,
+								  GIST_ROOT_BLKNO);
+		SetLastWrittenLSNForRelation(lsn, state->indexrel->rd_smgr->smgr_rnode.node.relNode);
 	}
 
 	pfree(pagestate->page);
