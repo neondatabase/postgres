300604d66ff689581ed68b52964c8b50b553a628
Konstantin Knizhnik <knizhnik@garret.ru>
2022-07-19 07:50:00 +0300

    Update last written LSN for gin/gist index metadata (#182)
    * Update last written LSN for gin/gist index metadata
    
    * Replace SetLastWrittenLSN with family of SetLastWrittenLSNFFor* functions

--- src/backend/commands/dbcommands.c
+++ src/backend/commands/dbcommands.c
@@ -685,7 +685,7 @@ createdb(ParseState *pstate, const CreatedbStmt *stmt)
 
 				lsn = XLogInsert(RM_DBASE_ID,
 								 XLOG_DBASE_CREATE | XLR_SPECIAL_REL_UPDATE);
-				SetLastWrittenLSN(lsn, InvalidOid, 0, 0);
+				SetLastWrittenLSNForDatabase(lsn);
 			}
 		}
 		table_endscan(scan);
@@ -2363,8 +2363,7 @@ dbase_redo(XLogReaderState *record)
 		 */
 		{
 			XLogRecPtr	lsn = record->EndRecPtr;
-
-			SetLastWrittenLSN(lsn, InvalidOid, 0, 0);
+			SetLastWrittenLSNForDatabase(lsn);
 		}
 	}
 	else if (info == XLOG_DBASE_DROP)
