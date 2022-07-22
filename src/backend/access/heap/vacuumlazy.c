4e3c69e25813633279e798e4a206f0094e7228ba
Konstantin Knizhnik <knizhnik@garret.ru>
2022-07-22 16:03:57 +0300

    Revert change with ingoring visibility errors

--- src/backend/access/heap/vacuumlazy.c
+++ src/backend/access/heap/vacuumlazy.c
@@ -1490,7 +1490,7 @@ lazy_scan_heap(LVRelState *vacrel, VacuumParams *params, bool aggressive)
 			/* ZENITH-XXX: all visible hint is not wal-logged
 			 * FIXME: Replay visibilitymap changes in pageserver
 			 */
-			elog(DEBUG1, "page is not marked all-visible but visibility map bit is set in relation \"%s\" page %u",
+			elog(FATAL, "page is not marked all-visible but visibility map bit is set in relation \"%s\" page %u",
 				 vacrel->relname, blkno);
 			visibilitymap_clear(vacrel->rel, blkno, vmbuffer,
 								VISIBILITYMAP_VALID_BITS);
