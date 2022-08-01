2427e4bbc0dfb6bf2f7688c56850291864246488
Konstantin Knizhnik <knizhnik@garret.ru>
2022-08-01 22:55:54 +0300

    Merge wiht main

--- contrib/zenith/zenith--1.0.sql
+++
@@ -1,7 +0,0 @@
-\echo Use "CREATE EXTENSION zenith" to load this file. \quit
-
-CREATE FUNCTION pg_cluster_size()
-RETURNS bigint
-AS 'MODULE_PATHNAME', 'pg_cluster_size'
-LANGUAGE C STRICT
-PARALLEL UNSAFE;
