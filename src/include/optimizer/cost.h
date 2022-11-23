6fce20cd50c46498cdfe511776fda79fca47b4d6
MMeent <matthias@neon.tech>
2022-11-23 12:55:14 +0100

    PG15: Prefetch cleanup (#241)
    * Update prefetch mechanisms:
    
    - **Enable enable_seqscan_prefetch by default**
    - Store prefetch distance in the relevant scan structs
    - Slow start sequential scan, to accommodate LIMIT clauses.
    - Replace seqscan_prefetch_buffer with the relations' tablespaces'
      *_io_concurrency; and drop seqscan_prefetch_buffer as a result.
    - Clarify enable_seqscan_prefetch GUC description
    - Fix prefetch in pg_prewarm
    - Add prefetching to autoprewarm worker
    - Fix an issue where we'd incorrectly not prefetch data when hitting a table wraparound. The same issue also resulted in assertion failures in debug builds.
    - Fix parallel scan prefetching - we didn't take into account that parallel scans have scan synchronization, too.

--- src/include/optimizer/cost.h
+++ src/include/optimizer/cost.h
@@ -70,7 +70,6 @@ extern PGDLLIMPORT bool enable_parallel_hash;
 extern PGDLLIMPORT bool enable_partition_pruning;
 extern PGDLLIMPORT bool enable_async_append;
 extern PGDLLIMPORT bool enable_seqscan_prefetch;
-extern PGDLLIMPORT int seqscan_prefetch_buffers;
 extern PGDLLIMPORT int constraint_exclusion;
 
 extern double index_pages_fetched(double tuples_fetched, BlockNumber pages,
