99e080d6cd1b9da1647c83f41703566069b87831
Konstantin Knizhnik <knizhnik@garret.ru>
2023-10-18 15:31:58 +0300

    Neon logical replication support for PG16 (#310)
    * Neon logical replication support for PG16
    
    * Log heap rewrite file after creation.
    
    ---------
    
    Co-authored-by: Konstantin Knizhnik <knizhnik@neon.tech>
    Co-authored-by: Arseny Sher <sher-ars@yandex.ru>

--- src/backend/replication/slot.c
+++ src/backend/replication/slot.c
@@ -47,6 +47,7 @@
 #include "miscadmin.h"
 #include "pgstat.h"
 #include "replication/slot.h"
+#include "replication/message.h"
 #include "storage/fd.h"
 #include "storage/ipc.h"
 #include "storage/proc.h"
@@ -684,6 +685,15 @@ ReplicationSlotDropPtr(ReplicationSlot *slot)
 	/* Generate pathnames. */
 	sprintf(path, "pg_replslot/%s", NameStr(slot->data.name));
 	sprintf(tmppath, "pg_replslot/%s.tmp", NameStr(slot->data.name));
+
+	if (SlotIsLogical(slot))
+	{
+		/* NEON specific: delete slot from storage using logical message */
+		char		prefix[MAXPGPATH];
+		snprintf(prefix, sizeof(prefix), "neon-file:%s/state", path);
+		elog(LOG, "Drop replication slot %s", path);
+		LogLogicalMessage(prefix, NULL, 0, false);
+	}
 
 	/*
 	 * Rename the slot directory on disk, so that we'll no longer recognize
@@ -1821,6 +1831,15 @@ SaveSlotToPath(ReplicationSlot *slot, const char *dir, int elevel)
 				(char *) (&cp) + ReplicationSlotOnDiskNotChecksummedSize,
 				ReplicationSlotOnDiskChecksummedSize);
 	FIN_CRC32C(cp.checksum);
+
+	if (SlotIsLogical(slot) && cp.slotdata.restart_lsn != InvalidXLogRecPtr)
+	{
+		/* NEON specific: persist slot in storage using logical message */
+		char		prefix[MAXPGPATH];
+		snprintf(prefix, sizeof(prefix), "neon-file:%s", path);
+		elog(LOG, "Save replication slot at %s restart_lsn=%X/%X", path, 	LSN_FORMAT_ARGS(cp.slotdata.restart_lsn));
+		LogLogicalMessage(prefix, (char*)&cp, sizeof cp, false);
+	}
 
 	errno = 0;
 	pgstat_report_wait_start(WAIT_EVENT_REPLICATION_SLOT_WRITE);
