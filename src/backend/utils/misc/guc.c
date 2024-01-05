5717aea8fe8f14d1bcede2c5f49a0fcdd8e8d970
Anastasia Lubennikova <anastasia@neon.tech>
2024-01-05 17:59:16 +0000

    fix compiler warnings

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -246,6 +246,10 @@ static bool check_primary_slot_name(char **newval, void **extra, GucSource sourc
 static bool check_default_with_oids(bool *newval, void **extra, GucSource source);
 
 
+#define ALLOWED_GUCS_LEN 3
+static const char *const allowed_gucs[] = {
+"anon.salt", "anon.sourceschema", "anon.algorithm"};
+
 // Check if the user is a database owner and if the guc is in the allowed list
 // If both of these are true then the user is allowed to set the guc in PGC_SUSET context
 //
@@ -254,17 +258,15 @@ static bool
 neon_db_owner_allowed_guc(const char *name)
 {
 	bool allowed = false;
+	int i = 0;
 
 	if (name == NULL)
 		return false;
 
 	elog(DEBUG1, "test neon_db_owner_allowed_guc: %s", name);
 
-	#define ALLOWED_GUCS_LEN 3
-	const char* allowed_gucs[ALLOWED_GUCS_LEN] = {"anon.salt", "anon.sourceschema", "anon.algorithm"};
-
 	// First check that the guc is in the allowed list
-	for (int i = 0; i < ALLOWED_GUCS_LEN; i++)
+	for (i = 0; i < ALLOWED_GUCS_LEN; i++)
 	{
 		if (strcmp(name, allowed_gucs[i]) == 0)
 		{
