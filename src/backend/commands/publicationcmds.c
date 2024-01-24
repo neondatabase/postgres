11e970fe2be56804f0a786ec5fc8141ffefa4ca7
Sasha Krassovsky <krassovskysasha@gmail.com>
2024-01-23 16:30:18 -0800

    Switch GetCurrentRoleId to GetUserId

--- src/backend/commands/publicationcmds.c
+++ src/backend/commands/publicationcmds.c
@@ -142,7 +142,7 @@ static bool
 is_neon_superuser(void)
 {
        Oid neon_superuser_oid = get_role_oid("neon_superuser", true /*missing_ok*/);
-       return neon_superuser_oid != InvalidOid && has_privs_of_role(GetCurrentRoleId(), neon_superuser_oid);
+       return neon_superuser_oid != InvalidOid && has_privs_of_role(GetUserId(), neon_superuser_oid);
 }
 
 /*
