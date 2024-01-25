3de48ce3d9c1f4fac1cdc7029487f8db9e537eac
Sasha Krassovsky <sasha@neon.tech>
2024-01-25 21:28:33 +0000

    Support creating subscriptions as neon_superuser

--- src/include/miscadmin.h
+++ src/include/miscadmin.h
@@ -373,6 +373,10 @@ extern void SetCurrentRoleId(Oid roleid, bool is_superuser);
 extern bool superuser(void);	/* current user is superuser */
 extern bool superuser_arg(Oid roleid);	/* given user is superuser */
 
+/* in utils/adt/acl.c */
+extern bool is_neon_superuser(void); /* current user is neon_superuser */
+extern bool is_neon_superuser_arg(Oid roleid); /* given user is neon_superuser */
+
 
 /*****************************************************************************
  *	  pmod.h --																 *
