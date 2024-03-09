neondatabase/postgres

commit 6969d90ea99f49c38d14820458b70c8c5b6db634

Treat walproposer like walsenders in postmaster.
This keeps the walproposer processes alive at shutdown, until after
the shutdown checkpoint has been written. That gives the walproposers
a chance to stream it to the safekeepers.

src/backend/postmaster/postmaster.c
@@ -4060,7 +4060,8 @@ SignalSomeChildren(int signal, int target)
 			 * Assign bkend_type for any recently announced WAL Sender
 			 * processes.
 			 */
-			if (bp->bkend_type == BACKEND_TYPE_NORMAL &&
+			/* Neon: Also allow walproposer background worker to be treated like a WAL sender, so that it's shut down last */
+			if ((bp->bkend_type == BACKEND_TYPE_NORMAL || BACKEND_TYPE_BGWORKER) &&
 				IsPostmasterChildWalSender(bp->child_slot))
 				bp->bkend_type = BACKEND_TYPE_WALSND;
 
@@ -5305,7 +5306,8 @@ CountChildren(int target)
 			 * Assign bkend_type for any recently announced WAL Sender
 			 * processes.
 			 */
-			if (bp->bkend_type == BACKEND_TYPE_NORMAL &&
+			/* Neon: Also allow walproposer background worker to be treated like a WAL sender, so that it's shut down last */
+			if ((bp->bkend_type == BACKEND_TYPE_NORMAL || BACKEND_TYPE_BGWORKER) &&
 				IsPostmasterChildWalSender(bp->child_slot))
 				bp->bkend_type = BACKEND_TYPE_WALSND;
 

commit 6a224dd68a2f863d6fe63fb26b7857558075329c

Fix bug introduced in 6969d90ea99f

src/backend/postmaster/postmaster.c
@@ -4061,7 +4061,7 @@ SignalSomeChildren(int signal, int target)
 			 * processes.
 			 */
 			/* Neon: Also allow walproposer background worker to be treated like a WAL sender, so that it's shut down last */
-			if ((bp->bkend_type == BACKEND_TYPE_NORMAL || BACKEND_TYPE_BGWORKER) &&
+			if ((bp->bkend_type == BACKEND_TYPE_NORMAL || bp->bkend_type == BACKEND_TYPE_BGWORKER) &&
 				IsPostmasterChildWalSender(bp->child_slot))
 				bp->bkend_type = BACKEND_TYPE_WALSND;
 
@@ -5307,7 +5307,7 @@ CountChildren(int target)
 			 * processes.
 			 */
 			/* Neon: Also allow walproposer background worker to be treated like a WAL sender, so that it's shut down last */
-			if ((bp->bkend_type == BACKEND_TYPE_NORMAL || BACKEND_TYPE_BGWORKER) &&
+			if ((bp->bkend_type == BACKEND_TYPE_NORMAL || bp->bkend_type == BACKEND_TYPE_BGWORKER) &&
 				IsPostmasterChildWalSender(bp->child_slot))
 				bp->bkend_type = BACKEND_TYPE_WALSND;
 