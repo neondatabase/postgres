5b11ebc8e749dfb24223033ba8c6115f2482f16d
Konstantin Knizhnik <knizhnik@neon.tech>
2024-06-24 17:58:03 +0300

    Enable writing WAL during shutdown checkpoint

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -692,7 +692,6 @@ static void CreateEndOfRecoveryRecord(void);
 static XLogRecPtr CreateOverwriteContrecordRecord(XLogRecPtr aborted_lsn,
 												  XLogRecPtr missingContrecPtr,
 												  TimeLineID newTLI);
-static void PreCheckPointGuts(int flags);
 static void CheckPointGuts(XLogRecPtr checkPointRedo, int flags);
 static void KeepLogSeg(XLogRecPtr recptr, XLogSegNo *logSegNo);
 static XLogRecPtr XLogGetReplicationSlotMinimumLSN(void);
@@ -6694,11 +6693,6 @@ CreateCheckPoint(int flags)
 	SyncPreCheckpoint();
 
 	/*
-	 * NEON: perform checkpiont action requiring write to the WAL before we determine the REDO pointer.
-	 */
-	PreCheckPointGuts(flags);
-
-	/*
 	 * Use a critical section to force system panic if we have trouble.
 	 */
 	START_CRIT_SECTION();
@@ -6963,7 +6957,15 @@ CreateCheckPoint(int flags)
 		if (flags & CHECKPOINT_END_OF_RECOVERY)
 			LocalXLogInsertAllowed = oldXLogAllowed;
 		else
-			LocalXLogInsertAllowed = 0; /* never again write WAL */
+		{
+			/*
+			 * NEON: fixme we need to persist some informastion on shutdown checkpoint
+			 * using AUX files mechanism (logical messages). So we need to be able to write WAL.
+			 */
+#if 0
+			 LocalXLogInsertAllowed = 0; /* never again write WAL */
+#endif
+		}
 	}
 
 	/*
@@ -7212,28 +7214,6 @@ CreateOverwriteContrecordRecord(XLogRecPtr aborted_lsn, XLogRecPtr pagePtr,
 	return recptr;
 }
 
-static void
-CheckPointReplicationState(void)
-{
-	CheckPointRelationMap();
-	CheckPointReplicationSlots();
-	CheckPointSnapBuild();
-	CheckPointLogicalRewriteHeap();
-	CheckPointReplicationOrigin();
-}
-
-/*
- * NEON:  we use logical records to persist information of about slots, origins, relation map...
- * If it is done inside shutdown checkpoint, then Postgres panics: "concurrent write-ahead log activity while database system is shutting down"
- * So it before checkpoint REDO position is determined.
- */
-static void
-PreCheckPointGuts(int flags)
-{
-	if (flags & CHECKPOINT_IS_SHUTDOWN)
-		CheckPointReplicationState();
-}
-
 /*
  * Flush all data in shared memory to disk, and fsync
  *
@@ -7243,8 +7223,11 @@ PreCheckPointGuts(int flags)
 static void
 CheckPointGuts(XLogRecPtr checkPointRedo, int flags)
 {
-	if (!(flags & CHECKPOINT_IS_SHUTDOWN))
-		CheckPointReplicationState();
+	CheckPointRelationMap();
+	CheckPointReplicationSlots();
+	CheckPointSnapBuild();
+	CheckPointLogicalRewriteHeap();
+	CheckPointReplicationOrigin();
 
 	/* Write out all dirty data in SLRUs and the main buffer pool */
 	TRACE_POSTGRESQL_BUFFER_CHECKPOINT_START(flags);
