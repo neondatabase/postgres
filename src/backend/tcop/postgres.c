6dc9a3f90849889635f7fd1a97dbb9f0ca852258
Matthias van de Meent <boekewurm+postgres@gmail.com>
2024-07-31 14:22:58 +0200

    NEON: Expose lag tracker, CHECK_FOR_INTERRUPTS, add backpressure

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -170,6 +170,8 @@ static volatile sig_atomic_t RecoveryConflictPendingReasons[NUM_PROCSIGNALS];
 static MemoryContext row_description_context = NULL;
 static StringInfoData row_description_buf;
 
+process_interrupts_callback_t ProcessInterruptsCallback;
+
 /* ----------------------------------------------------------------
  *		decls for routines only used in this file
  * ----------------------------------------------------------------
@@ -3247,6 +3249,7 @@ ProcessInterrupts(void)
 		return;
 	InterruptPending = false;
 
+retry:
 	if (ProcDiePending)
 	{
 		ProcDiePending = false;
@@ -3469,6 +3472,13 @@ ProcessInterrupts(void)
 
 	if (ParallelApplyMessagePending)
 		HandleParallelApplyMessages();
+
+	/* Call registered callback if any */
+	if (ProcessInterruptsCallback)
+	{
+		if (ProcessInterruptsCallback())
+			goto retry;
+	}
 }
 
 /*
