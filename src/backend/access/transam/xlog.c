78027f8528ba03a0a640eeb7384cfef9e936d6d6
Anastasia Lubennikova <anastasia@neon.tech>
2024-08-09 00:00:02 +0100

    Remove assertion. TODO figure out why and add a comment

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6698,7 +6698,8 @@ GetRedoStartLsn(void)
 XLogRecPtr
 GetFlushRecPtr(TimeLineID *insertTLI)
 {
-	Assert(XLogCtl->SharedRecoveryState == RECOVERY_STATE_DONE);
+	// NEON
+	//Assert(XLogCtl->SharedRecoveryState == RECOVERY_STATE_DONE);
 
 	RefreshXLogWriteResult(LogwrtResult);
 
