a4830163a65811578824ce4022c1cd3daef33d4e
Konstantin Knizhnik <knizhnik@neon.tech>
2024-09-30 09:20:34 +0300

    Add custom hook to checkpoint

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -140,6 +140,8 @@ bool		track_wal_io_timing = false;
 uint64      predefined_sysidentifier;
 int			lastWrittenLsnCacheSize;
 
+CustomCheckpointHookType CustomCheckpointHook;
+
 #ifdef WAL_DEBUG
 bool		XLOG_DEBUG = false;
 #endif
@@ -7230,6 +7232,8 @@ CheckPointReplicationState(void)
 static void
 PreCheckPointGuts(int flags)
 {
+	if (CustomCheckpointHook)
+		CustomCheckpointHook(flags);
 	if (flags & CHECKPOINT_IS_SHUTDOWN)
 		CheckPointReplicationState();
 }
