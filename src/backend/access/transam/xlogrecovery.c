feb4e84052ba857032d894bf271a932b2e180e25
Konstantin Knizhnik <knizhnik@neon.tech>
2024-11-28 19:12:16 +0200

    Fix condition for pausing replciation

--- src/backend/access/transam/xlogrecovery.c
+++ src/backend/access/transam/xlogrecovery.c
@@ -91,7 +91,7 @@ TimestampTz recoveryTargetTime;
 const char *recoveryTargetName;
 XLogRecPtr	recoveryTargetLSN;
 int			recovery_min_apply_delay = 0;
-bool		allowReplicaMisconfig = false;
+bool		allowReplicaMisconfig = false; /* NEON: GUC is defined in neon extension */
 
 /* options formerly taken from recovery.conf for XLOG streaming */
 char	   *PrimaryConnInfo = NULL;
@@ -4862,9 +4862,9 @@ RecoveryRequiresIntParameter(const char *param_name, int currValue, int minValue
 							   currValue,
 							   minValue)));
 
-			if (!allowReplicaMisconfig)
+			if (allowReplicaMisconfig)
 			{
-				/* Сontinue replication even though it can cause problerms later */
+				/* Сontinue replication even though it can cause problems later */
 				return;
 			}
 
