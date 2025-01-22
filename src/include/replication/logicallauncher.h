68a01af4f97e103e411ef6f577c3915cb12c478a
Anastasia Lubennikova <anastasia@neon.tech>
2025-01-22 13:25:56 +0000

    Add new GUC disable_logical_replication_subscribers (#567)

--- src/include/replication/logicallauncher.h
+++ src/include/replication/logicallauncher.h
@@ -16,6 +16,8 @@ extern PGDLLIMPORT int max_logical_replication_workers;
 extern PGDLLIMPORT int max_sync_workers_per_subscription;
 extern PGDLLIMPORT int max_parallel_apply_workers_per_subscription;
 
+extern PGDLLIMPORT bool disable_logical_replication_subscribers;
+
 extern void ApplyLauncherRegister(void);
 extern void ApplyLauncherMain(Datum main_arg);
 
