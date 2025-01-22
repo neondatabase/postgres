96a42936ceb82a4649820227de8c2b10333017c0
Anastasia Lubennikova <anastasia@neon.tech>
2025-01-22 13:25:56 +0000

    Add new GUC disable_logical_replication_subscribers (#567)

--- src/backend/replication/logical/launcher.c
+++ src/backend/replication/logical/launcher.c
@@ -51,6 +51,8 @@ int			max_logical_replication_workers = 4;
 int			max_sync_workers_per_subscription = 2;
 int			max_parallel_apply_workers_per_subscription = 2;
 
+bool disable_logical_replication_subscribers = false; /* NEON: GUC is defined in neon extension */
+
 LogicalRepWorker *MyLogicalRepWorker = NULL;
 
 typedef struct LogicalRepCtxStruct
@@ -332,6 +334,15 @@ logicalrep_worker_launch(LogicalRepWorkerType wtype,
 		ereport(ERROR,
 				(errcode(ERRCODE_CONFIGURATION_LIMIT_EXCEEDED),
 				 errmsg("cannot start logical replication workers when max_replication_slots = 0")));
+
+	/* NEON GUC that allows to control logical replication without postgres restart */
+	if (disable_logical_replication_subscribers)
+	{
+		ereport(WARNING,
+				(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
+				 errmsg("cannot start logical replication workers when disable_logical_replication_subscribers=true")));
+		return false;
+	}
 
 	/*
 	 * We need to do the modification of the shared memory under lock so that
