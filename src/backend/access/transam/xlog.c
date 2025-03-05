8de5b9ac12e8a740de9304dd00fa955e624d7cdd
Suhas Thalanki <suhastvs@gmail.com>
2025-03-05 10:27:14 -0500

    fixed return statement for block range

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6660,7 +6660,6 @@ SetLastWrittenLSNForBlockRange(XLogRecPtr lsn, RelFileLocator rlocator, ForkNumb
 	if (set_lwlsn_block_range_hook) 
 	{
 		return set_lwlsn_block_range_hook(lsn, rlocator, forknum, from, n_blocks);
-		return set_lwlsn_block_range_hook(lsn, rlocator, forknum, from, n_blocks);
 	}
 	return lsn;
 }
