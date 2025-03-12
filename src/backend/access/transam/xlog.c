faff7e3b457265c672fd04cb71d364f5ec36ea74
Suhas Thalanki <suhastvs@gmail.com>
2025-03-12 13:26:04 -0400

    removed unnecesary hooks

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6637,14 +6637,9 @@ GetInsertRecPtr(void)
 	return recptr;
 }
 
-get_lwlsn_hook_type get_lwlsn_hook = NULL;
-get_lwlsn_v_hook_type get_lwlsn_v_hook = NULL;
 set_lwlsn_block_range_hook_type set_lwlsn_block_range_hook = NULL;
 set_lwlsn_block_v_hook_type set_lwlsn_block_v_hook = NULL;
 set_lwlsn_block_hook_type set_lwlsn_block_hook = NULL;
-set_lwlsn_relation_hook_type set_lwlsn_relation_hook = NULL;
-set_lwlsn_db_hook_type set_lwlsn_db_hook = NULL;
-get_lwlsn_cache_size_type get_lwlsn_cache_size = NULL;
 
 /*
  * SetLastWrittenLSNForBlockRange -- Set maximal LSN of written page range.
@@ -6715,11 +6710,7 @@ SetLastWrittenLSNForBlock(XLogRecPtr lsn, RelFileLocator rlocator, ForkNumber fo
 XLogRecPtr
 SetLastWrittenLSNForRelation(XLogRecPtr lsn, RelFileLocator rlocator, ForkNumber forknum)
 {
-	if (set_lwlsn_relation_hook)
-	{
-		return set_lwlsn_relation_hook(lsn, rlocator, forknum);
-	}
-	return lsn;
+	return SetLastWrittenLSNForBlock(lsn, rlocator, forknum, REL_METADATA_PSEUDO_BLOCKNO);
 }
 
 /*
@@ -6728,11 +6719,8 @@ SetLastWrittenLSNForRelation(XLogRecPtr lsn, RelFileLocator rlocator, ForkNumber
 XLogRecPtr
 SetLastWrittenLSNForDatabase(XLogRecPtr lsn)
 {
-	if (set_lwlsn_db_hook)
-	{
-		return set_lwlsn_db_hook(lsn);
-	}
-	return lsn;
+	RelFileLocator dummyNode = {InvalidOid, InvalidOid, InvalidOid};
+	return SetLastWrittenLSNForBlock(lsn, dummyNode, MAIN_FORKNUM, 0);
 }
 
 void
