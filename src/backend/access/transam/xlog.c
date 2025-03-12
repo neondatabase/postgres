409da40fe97a62853fcc59132e2481d0680aa5ec
Suhas Thalanki <suhastvs@gmail.com>
2025-03-12 11:36:24 -0400

    added hook to update maxlastwrittenlsn

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -4991,6 +4991,8 @@ readZenithSignalFile(void)
 	}
 }
 
+update_max_lwlsn_hook_type update_max_lwlsn_hook = NULL;
+
 /*
  * This must be called ONCE during postmaster or standalone-backend startup
  */
@@ -5258,6 +5260,10 @@ StartupXLOG(void)
 
 	RedoRecPtr = XLogCtl->RedoRecPtr = XLogCtl->Insert.RedoRecPtr = checkPoint.redo;
 	doPageWrites = lastFullPageWrites;
+
+	if (update_max_lwlsn_hook) {
+		update_max_lwlsn_hook(RedoRecPtr);
+	}
 
 	/* REDO */
 	if (InRecovery)
