15811b5fe94d5f45ff78fc2f012c4bcdef926083
Konstantin Knizhnik <knizhnik@neon.tech>
2025-06-30 22:24:21 +0300

    Reintialize page in allocNewBuffer only when buffer is returned

--- src/backend/access/spgist/spgutils.c
+++ src/backend/access/spgist/spgutils.c
@@ -525,10 +525,10 @@ allocNewBuffer(Relation index, int flags)
 		Buffer		buffer;
 
 		buffer = SpGistNewBuffer(index);
-		SpGistInitBuffer(buffer, pageflags);
 
 		if (pageflags & SPGIST_LEAF)
 		{
+			SpGistInitBuffer(buffer, pageflags);
 			/* Leaf pages have no parity concerns, so just use it */
 			return buffer;
 		}
@@ -539,6 +539,7 @@ allocNewBuffer(Relation index, int flags)
 
 			if ((flags & GBUF_PARITY_MASK) == blkFlags)
 			{
+				SpGistInitBuffer(buffer, pageflags);
 				/* Page has right parity, use it */
 				return buffer;
 			}
