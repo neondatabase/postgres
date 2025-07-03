d1627bff1c5f8172845406a3b85b7aa84a7760e2
Konstantin Knizhnik <knizhnik@neon.tech>
2025-07-03 14:24:20 +0300

    Do not invalidate obsolete slot if on demand wal download is supported

--- src/backend/replication/slot.c
+++ src/backend/replication/slot.c
@@ -44,6 +44,7 @@
 #include "common/string.h"
 #include "miscadmin.h"
 #include "pgstat.h"
+#include "replication/logical.h"
 #include "replication/slot.h"
 #include "replication/message.h"
 #include "storage/fd.h"
@@ -1281,7 +1282,7 @@ InvalidatePossiblyObsoleteSlot(ReplicationSlot *s, XLogRecPtr oldestLSN,
 		 * If the slot is already invalid or is fresh enough, we don't need to
 		 * do anything.
 		 */
-		if (XLogRecPtrIsInvalid(restart_lsn) || restart_lsn >= oldestLSN)
+		if (XLogRecPtrIsInvalid(restart_lsn) || restart_lsn >= oldestLSN || Custom_XLogReaderRoutines)
 		{
 			SpinLockRelease(&s->mutex);
 			if (released_lock)
