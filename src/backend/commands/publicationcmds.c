b788e8825a51d75ef956d1af2386620c4773c1d0
Alexey Kondratov <kondratov.aleksey@gmail.com>
2025-07-11 23:20:06 +0200

    Parametrize neon_superuser via privileged_role_name var

--- src/backend/commands/publicationcmds.c
+++ src/backend/commands/publicationcmds.c
@@ -748,7 +748,7 @@ CreatePublication(ParseState *pstate, CreatePublicationStmt *stmt)
 					   get_database_name(MyDatabaseId));
 
 	/* FOR ALL TABLES requires superuser */
-	if (stmt->for_all_tables && !superuser() && !is_neon_superuser())
+	if (stmt->for_all_tables && !superuser() && !is_privileged_role())
 		ereport(ERROR,
 				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
 				 errmsg("must be superuser to create FOR ALL TABLES publication")));
@@ -819,7 +819,7 @@ CreatePublication(ParseState *pstate, CreatePublicationStmt *stmt)
 								   &schemaidlist);
 
 		/* FOR TABLES IN SCHEMA requires superuser */
-		if (schemaidlist != NIL && !superuser() && !is_neon_superuser())
+		if (schemaidlist != NIL && !superuser() && !is_privileged_role())
 			ereport(ERROR,
 					errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
 					errmsg("must be superuser to create FOR TABLES IN SCHEMA publication"));
