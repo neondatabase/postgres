e5bac9c3bf1d21aa7f5a8e04be3e5ccf03dd9afb
Haoyu Huang <haoyu.huang@databricks.com>
2025-09-16 21:33:59 +0000

    c

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -129,6 +129,7 @@ restore_running_xacts_callback_t restore_running_xacts_callback;
 set_lwlsn_db_hook_type set_lwlsn_db_hook = NULL;
 set_lwlsn_relation_hook_type set_lwlsn_relation_hook = NULL;
 set_max_lwlsn_hook_type set_max_lwlsn_hook = NULL;
+flush_dirty_buffers_hook_type flush_dirty_buffers_hook = NULL;
 
 /*
  * Number of WAL insertion locks to use. A higher value allows more insertions
@@ -10156,16 +10157,18 @@ CreateRestartPoint(int flags)
 			ControlFile->time = (pg_time_t) time(NULL);
 			UpdateControlFile();
 			LWLockRelease(ControlFileLock);
-			// Flush dirty buffers.
-			CheckPointBuffers(flags);
+
+			if (flush_dirty_buffers_hook)
+			{
+				flush_dirty_buffers_hook(flags);
+			}
 		}
 		return false;
 	}
 
-	if (flags & CHECKPOINT_IS_SHUTDOWN)
+	if (flush_dirty_buffers_hook)
 	{
-		// Flush dirty buffers.
-		CheckPointBuffers(flags);
+		flush_dirty_buffers_hook(flags);
 	}
 
 	/*
