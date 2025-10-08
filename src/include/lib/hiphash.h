e51edaf2982e41f42f3c790a4ff39eb1afeedb90
Matthias van de Meent <boekewurm+postgres@gmail.com>
2025-10-08 09:44:13 +0200

    Introduce HIPHash shared hash tables
    They will have more efficient memory usage and lookups than Dynahash.c,
    whilst still being shardable and having decent shared memory semantics.
    
    Implementation TBD.

---
+++ src/include/lib/hiphash.h
@@ -0,0 +1,51 @@
+/*-------------------------------------------------------------------------
+ *
+ * hiphash.h
+ *	  Concurrent hash table backed by a single shared memory allocation.
+ *
+ * Copyright (c) 2025, PostgreSQL Global Development Group
+ *
+ * IDENTIFICATION
+ *	  src/include/lib/hiphash.h
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef HIPHASH_H
+#define HIPHASH_H
+
+typedef struct HIPHashHeader HIPHashHeader;
+typedef int32 HIPEntryIndex;
+
+Size HIPGetSize(int nelements);
+void HIPInit(HIPHashHeader *header, int nelements);
+
+/* Get this element from the table. No locks involved. */
+HIPEntryIndex HIPGetElementUnchecked(HIPHashHeader *header, uint32 hash);
+/*
+ * Find this exact element in the provided elemsarray with element size
+ * of stride and compare prefix of cmpsize, offset by HIPEntryIndex.
+ *
+ * Requires lock. Returns NULL when not found.
+ */
+void * HIPGetElementChecked(HIPHashHeader *header, uint32 hash,
+							void *searchelem, void *elemsarray,
+							Size stride, Size cmpsize);
+
+/*
+ * Insert this entry into the HIP hash table.
+ *
+ * Caller is responsible for locking.
+ */
+HIPEntryIndex HIPInsertElement(HIPHashHeader *header, uint32 hash,
+							   HIPEntryIndex entry);
+
+/*
+ * Delete this element from the hash table.
+ *
+ * Caller is responsible for locking.
+ */
+void HIPRemoveElement(HIPHashHeader *header, uint32 hash,
+					  HIPEntryIndex entry);
+
+#endif /* HIPHASH_H */
