cce396cd998a047196d3c18575a282c6d5463741
Yanan Xin <yanan.xin@databricks.com>
2025-12-05 22:59:55 +0000

    [LKB-5974][PG_18] Add the ability to reduce FPI

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -863,6 +863,10 @@ XLogInsertRecord(XLogRecData *rdata,
 		 * our local copy but not force a recomputation.  (If doPageWrites was
 		 * just turned off, we could recompute the record without full pages,
 		 * but we choose not to bother.)
+		 *
+		 * However, if suppress_fpi is true, we skip the recomputation check
+		 * since we're deliberately suppressing full page images for this
+		 * record via the FPI control hook.
 		 */
 		if (RedoRecPtr != Insert->RedoRecPtr)
 		{
@@ -873,7 +877,8 @@ XLogInsertRecord(XLogRecData *rdata,
 
 		if (doPageWrites &&
 			(!prevDoPageWrites ||
-			 (fpw_lsn != InvalidXLogRecPtr && fpw_lsn <= RedoRecPtr)))
+			 (!suppress_fpi &&
+			  fpw_lsn != InvalidXLogRecPtr && fpw_lsn <= RedoRecPtr)))
 		{
 			/*
 			 * Oops, some buffer now needs to be backed up that the caller
