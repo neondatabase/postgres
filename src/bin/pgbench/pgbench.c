dad3da8d7f643b8d635510924ce4216bfb83e973
Alexander Bayandin <alexander@neon.tech>
2026-01-14 12:18:14 +0000

    pgbench: fix `-Wunused-but-set-variable` warning

--- src/bin/pgbench/pgbench.c
+++ src/bin/pgbench/pgbench.c
@@ -7822,8 +7822,6 @@ main(int argc, char **argv)
 	 */
 	if (report_percentiles && report_per_command)
 	{
-		int			total_commands = 0;
-
 		for (i = 0; i < num_scripts; i++)
 		{
 			Command   **commands = sql_script[i].commands;
@@ -7832,7 +7830,6 @@ main(int argc, char **argv)
 			{
 				commands[j]->latency_hist = (LatencyHistogram *) pg_malloc(sizeof(LatencyHistogram));
 				initLatencyHistogram(commands[j]->latency_hist);
-				total_commands++;
 			}
 		}
 	}
